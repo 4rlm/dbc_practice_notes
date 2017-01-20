1a011f5c725aa3f2722ffb921b714b0a973248e0
Adam Booth <4rlm@protonmail.ch>
2017-01-20 13:53:00 -0600

    started 6.5 Mandatory Pairing: Testing a Class with RSpec

---
+++ 6.5_pairing_testing_class_w_rspec.rb
@@ -0,0 +1,7 @@
+## 6.5 Mandatory Pairing: Testing a Class with RSpec ##
+## Related to ruby/calculator/calculator.rb & calculator_spec.rb
+
+#Terms & Tips:
+# file should end in _spec.rb and be in spec subdirectory of project.
+# Red to Green: Process of repeatedly starting with a failing test and then making it pass.
+# TDD: Test Driven Development
